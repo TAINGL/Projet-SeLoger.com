8f0d30f1f1c739daff9901ded826b0476746ec5c
Laura TAING <48871171+TAINGL@users.noreply.github.com>
2020-06-08 23:02:53 +0200

    Update SeLoger-app

--- application/seloger_app.py
+++ application/seloger_app.py
@@ -44,7 +44,7 @@ def get_data(dic, column):
     seloger =  pd.read_csv(dic[column], index_col = 0)
     y = seloger[target]# get labels
     X = seloger.drop(target, axis=1) #
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
     return seloger, X_train, X_test, y_train, y_test
 
 
